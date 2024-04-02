7b8b4d91ea5f1a9088dfac6d87c03ba53e68a180
Trần Minh Châu <dluffy2332@gmail.com>
2024-04-02 21:25:27 +0700

    init project

---
+++ confferal/src/test/java/com/ptit/dev/confferal/ConfferalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit.dev.confferal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfferalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
