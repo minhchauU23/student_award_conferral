753d0cd040ec69b992390a14c4a954050de813e7
bxduy <buixuanduy1301@gmail.com>
2024-04-17 00:16:19 +0700

    verify application

---
+++ verify_application/src/test/java/com/devptit/verify_application/VerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devptit.verify_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
