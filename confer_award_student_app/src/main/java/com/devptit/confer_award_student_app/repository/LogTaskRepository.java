a897f29f8dfd6596139a269d12ee2a505b73ce2e
minhchauU23 <dluffy2332@gmail.com>
2024-05-08 04:23:48 +0700

    del confer

--- confer_award_student_app/src/main/java/com/devptit/confer_award_student_app/repository/LogTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devptit.confer_award_student_app.repository;
-
-import com.devptit.confer_award_student_app.entity.LogTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogTaskRepository extends JpaRepository<LogTask, String > {
-}
--- confer_award_student_app/src/main/java/com/devptit/confer_award_student_app/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devptit.confer_award_student_app.repository;
-
-import com.devptit.confer_award_student_app.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, String > {
-}
